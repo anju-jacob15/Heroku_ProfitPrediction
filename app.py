2b551ec65dbf23b7b8898af1c1a58950046d06a0
anju-jacob15 <87362515+anju-jacob15@users.noreply.github.com>
2021-07-17 22:44:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask,request,jsonify,render_template
+
+# start flask
+app = Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+# render default webpage
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    rd=request.form['rd']
+    ad=request.form['administration']
+    mar=request.form['marketing']
+    st=request.form['state']
+    a=[]
+    if(st=='Florida'or st=='florida'):
+        a=[1,0]
+    if(st=='California'or st=='california'):
+        a=[0,0]
+    if(st=='New York'or st=='new york'):
+        a=[0,1]
+    prediction=model.predict([[float(rd),float(ad),float(mar),a[0],a[1]]])
+    
+    output=round(prediction[0],2)
+    
+    return render_template('index.html',prediction_text='Profit is Rs. {}'.format(output))
+    
+if __name__ == '__main__':
+    app.run(debug=True)
